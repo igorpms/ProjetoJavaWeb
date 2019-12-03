78c1083fad1310d26b9d493f8482f4da868e1687
Igor Marques Santiago <49252230+igorpms@users.noreply.github.com>
2019-12-03 13:21:26 -0300

    Upload segunda entrega

---
+++ agenda/src/main/java/br/com/agenda/controller/LoginController.java
@@ -0,0 +1,47 @@
+package br.com.agenda.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginController
+ */
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+		
+		/**
+		 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+		 */
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			    throws ServletException, IOException {
+			        doPost(request, response);
+		}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String senha = request.getParameter("senha");
+		System.out.println("A email é:" + email + "\n" + "A senha é:" + senha);
+		if("igor@hotmail.com".equals(email) && "igor123".equals(senha)) {
+			RequestDispatcher rd = request.getRequestDispatcher("Login.jsp");
+			rd.forward(request, response);
+		}else {
+			response.sendRedirect("Login.jsp");
+		}
+	}
+
+}
---
+++ agenda/target/m2e-wtp/web-resources/Login.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<meta name="viewport"
+	content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>Login</title>
+
+
+<link href="${request.contextPath}bootstrap/css/bootstrap.min.css"
+	rel="stylesheet">
+<link href="${request.contextPath}bootstrap/css/signin.css"
+	rel="stylesheet">
+</head>
+
+<body class="text-center">
+	<form class="form-signin" action="LoginController" method="post">
+
+		<label for="email" class="sr-only">Endereço de email</label> <input
+			type="email" name="email" id="email" class="form-control"
+			placeholder="Email" required autofocus> <br/> <label
+			for="inputPassword" class="sr-only">Senha</label> <input
+			type="password" name="senha" id="senha" class="form-control"
+			placeholder="Senha" required>
+
+		<button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+	</form>
+</body>
+</html>
